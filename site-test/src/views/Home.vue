eba50aaa67743a41078ef7bcb7d892f77473d822
AureliaPiot <aurelia.piot@hotmail.com>
2021-11-01 20:17:52 +0100

    start

---
+++ site-test/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TestComponents msgTest="welp, this it a test" />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import TestComponents from '@/components/test-component-1.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    TestComponents
+  }
+}
+</script>
